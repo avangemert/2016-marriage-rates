34e6553e674f81e9fdbfbeedeb8c1efe76ba5c64
Allison van Gemert <avangemert@apple.com>
2018-08-17 09:39:59 -0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// Chart params
+var svgWidth = 750;
+var svgHeight = 750;
+
+var margin ={
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#bubble")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+var file = "data.csv"
+d3.csv(file).then(successHandle, errorHandle);
+
+function errorHandle(error){
+    throw err;
+}
+
+function successHandle(marriageData){
+
+    // Print the healthData
+    console.log(marriageData);
+
+    
+   
+}
