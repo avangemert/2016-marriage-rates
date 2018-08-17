8783e757f6dee329b35cdd6f1ea42d136c7a5cd0
Allison van Gemert <avangemert@apple.com>
2018-08-17 15:31:38 -0500

    background dummy image added

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,38 @@ var height = svgHeight - margin.top - margin.bottom;
 var svg = d3.select("#bubble")
     .append("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform", "translate(0,0)")
 
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var defs = svg.append("defs");
+
+defs.append("pattern")
+    .attr("id", "jon-snow")
+    .attr("height", "100%")
+    .attr("width", "100%")
+    .attr("patternContentUnits", "objectBoundingBox")
+    .append("image")
+    .attr("height", 1)
+    .attr("width", 1)
+    .attr("preserveAspectRatio", "none")
+    .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+    .attr("xlink:href", "snow.jpg")
+
+   
+// Scale the circles
+var radiusScale = d3.scaleSqrt().domain([5, 30]).range([10, 80])
+
+// Use the Force/prevent collisions
+var simulation = d3.forceSimulation()
+.force("x", d3.forceX(width / 2).strength(0.05))
+.force("y", d3.forceY(height / 2).strength(0.05))
+.force("collide", d3.forceCollide(function(d){
+    return radiusScale(d.rate) + 1;
+}))
 
 // Import Data
-var file = "data.csv"
+var file = "https://raw.githubusercontent.com/avangemert/interactive-d3-bubble-chart/master/assets/data/data.csv"
 d3.csv(file).then(successHandle, errorHandle);
 
 function errorHandle(error){
@@ -33,6 +58,48 @@ function successHandle(marriageData){
 
     // Print the healthData
     console.log(marriageData);
+
+    // Scale the circles
+    var radiusScale = d3.scaleSqrt().domain([5, 30]).range([10, 80])
+
+    // Initialize tooltip
+    var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .html(function(d) {
+        return(`<h6>${d.state}</h6>Marriage Rate: ${d.rate}%`);});
+    
+    // Create tooltip in the chart
+    svg.call(toolTip);
+
+    // Create circles
+    var circles = svg.selectAll(".state")
+    .data(marriageData)
+    .enter()
+    .append("circle")
+    .attr("class", "state")
+    .attr("r", function(d){
+        return radiusScale(d.rate);
+    })
+    .attr("fill", "url(#jon-snow)")
+    .on('click', function(d){
+        toolTip.show(d, event.target)
+            .direction("nw");
+    }).on("mouseout", function(d, index){
+        toolTip.hide(d, event.target);
+    });
+
+    simulation.nodes(marriageData)
+    .on('tick', ticked)
+
+    function ticked(){
+        circles
+        .attr("cx", function(d){
+            return d.x
+        })
+        .attr("cy", function(d){
+            return d.y
+        })
+    }
 
     
    
